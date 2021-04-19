87c3ca4017f86667617719f40537b1ba4d23c027
vegter <amsjurien@gmail.com>
2021-04-19 12:01:13 +0200

    Add age to state
    The age is incremented with each update
    Use age to display a notification to save the updated data
    so that changes do not get lost
    Display a warning on the upload page to prevent accidental data loss

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import accountingReducer from './AccountingSlice'
 import filterReducer from './FilterSlice'
+import ageReducer, { resetAge, increaseAge } from './AgeSlice'
 import { Accounting } from "../types/Accounting"
 
 const updateAccounting = (store: any) => (next: any) => (action: any) => {
@@ -18,13 +19,30 @@ const updateAccounting = (store: any) => (next: any) => (action: any) => {
   return result
 }
 
+const updateAge = ({ dispatch }: any) => (next: any) => (action: any) => {
+  // Execute action
+  const result = next(action)
+
+  if (/(restore|reset)(Accounting|Filter)State/.test(action.type)) {
+    // Reset age on any filter or accounting restore or reset action
+    dispatch(resetAge())
+  } else if (/^(accounting|filter)\//.test(action.type)  && !/^accounting\/addStatements/.test(action.type) ) {
+    // any other filter or accounting action increases the age
+    // Expect for reading statements (unmodified data loaded from an external file)
+    dispatch(increaseAge())
+  }
+
+  return result
+}
+
 export const store = configureStore({
   reducer: {
     accounting: accountingReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    age: ageReducer
   },
   middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(updateAccounting)
+      getDefaultMiddleware().concat([updateAccounting, updateAge])
 });
 
 export function setAccountingState(store: any) {
